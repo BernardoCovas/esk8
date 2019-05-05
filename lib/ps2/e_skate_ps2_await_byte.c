0d23f8295950e5c1b63285947dba8bb48100d2c8
Bernardo Covas <bernardo.covas@outlook.com>
2019-05-05 23:35:55 +0200

    bug fix

--- lib/ps2/e_skate_ps2_await_byte.c
+++ lib/ps2/e_skate_ps2_await_byte.c
@@ -14,7 +14,7 @@ e_skate_err_t e_skate_ps2_await_byte(
 )
 {
     bool received = xQueueReceive(
-        ps2Handle->byteQueueHandle,
+        ps2Handle->txByteQueueHandle,
         outByte,
         E_SKATE_PS2_BYTE_QUEUE_TIMEOUT_MS / portTICK_PERIOD_MS);
 
