e2181538ff3ca4103f696cb24aeb0fc716feadf8
Bernardo Covas <bernardo.covas@outlook.com>
2019-06-16 08:18:58 +0200

    Uart driver might already have been installed. Now ignoring this error

--- mcu/main/lib/bms/e_ride_bms_init.c
+++ mcu/main/lib/bms/e_ride_bms_init.c
@@ -10,27 +10,23 @@ e_ride_err_t e_ride_bms_init(
 
 )
 {
-    ESP_ERROR_CHECK(uart_param_config(
-        bmsConfig->bmsUartPort,
-        &bmsConfig->uartConfig
-        ));
+    ESP_ERROR_CHECK(uart_param_config(bmsConfig->bmsUartPort, &bmsConfig->uartConfig));
 
     ESP_ERROR_CHECK(uart_set_pin(
-        bmsConfig->bmsUartPort,
-        bmsConfig->batTxPin,
-        bmsConfig->batRxPins[0],
-        UART_PIN_NO_CHANGE,
-        UART_PIN_NO_CHANGE
-        ));
+        bmsConfig->bmsUartPort, bmsConfig->batTxPin,
+        bmsConfig->batRxPins[0], UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    ESP_ERROR_CHECK(uart_driver_install(
-        bmsConfig->bmsUartPort,
-        E_RIDE_UART_BMS_BUFF_SIZE,
-        0,
-        0,
-        NULL,
-        0
-        ));
+    esp_err_t errCode = uart_driver_install(
+        bmsConfig->bmsUartPort, E_RIDE_UART_BMS_BUFF_SIZE, 0, 0, NULL, 0);
+
+    /**
+     * BLE (and others) communicate through UART,
+     * so this might already be installed. In that
+     * case it returns ESP_FAIL (Instead of a specific error),
+     * and we can ignore it.
+     */
+    if (errCode != ESP_OK && errCode != ESP_FAIL)
+        ESP_ERROR_CHECK(errCode);
 
     return E_RIDE_SUCCESS;
 }
